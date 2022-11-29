6896c4df8515d06b1647ab5eb3fe220da98b15a0
Pavlína Hanzlová <pav.hanzlova@gmail.com>
2022-11-29 22:41:19 +0100

    create Day_01 project

---
+++ AdventOfCode_2021/Day_01/SonarSweep.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode_2021.Day_01 {
+    class SonarSweep {
+        }
+    }
